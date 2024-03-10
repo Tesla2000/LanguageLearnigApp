8396c6bf100c92d023efebe5c9f03b514a87d579
Tesla2000 <fratajczak124@gmail.com>
2024-03-10 22:31:39 +0100

    Added feedback

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -19,14 +19,109 @@ class MainActivity : AppCompatActivity() {
     private var userAnswer: String = ""
     private var askingSpeechInput = false
     private val repetitionRate = 0.5f
-    private val bufferSize = 5
+    private val bufferSize = 2
+	private val speechRate = 0.5f
     private val unansweredQuestionAnswerPairs: ArrayList<ArrayList<String>> = arrayListOf(
-        arrayListOf("mother", "die mutter"),
-        arrayListOf("father", "der vater"),
-        arrayListOf("cat", "die katze"),
-        arrayListOf("dog", "der hund"),
-        arrayListOf("brother", "der Bruder"),
-        arrayListOf("sister", "die Schwester"),
+		arrayListOf("the ability", "die Fähigkeit"),
+		arrayListOf("able", "fähig"),
+		arrayListOf("about", "um"),
+		arrayListOf("the above", "obenstehendes"),
+		arrayListOf("accept", "akzeptieren"),
+		arrayListOf("according", "nach"),
+		arrayListOf("the account", "das Konto"),
+		arrayListOf("across", "über"),
+		arrayListOf("the act", "Der Akt"),
+		arrayListOf("the action", "die Aktion"),
+		arrayListOf("the activity", "die Aktivität"),
+		arrayListOf("actually", "Genau genommen"),
+		arrayListOf("the add", "das Hinzufügen"),
+		arrayListOf("the address", "die Adresse"),
+		arrayListOf("the administration", "die Verwaltung"),
+		arrayListOf("admit", "zugeben"),
+		arrayListOf("the adult", "der Erwachsene"),
+		arrayListOf("the affect", "der Affekt"),
+		arrayListOf("after", "nach"),
+		arrayListOf("again", "wieder"),
+		arrayListOf("against", "gegen"),
+		arrayListOf("the age", "das Alter"),
+		arrayListOf("the agency", "die Agentur"),
+		arrayListOf("the agent", "Der Agent"),
+		arrayListOf("ago", "vor"),
+		arrayListOf("agree", "zustimmen"),
+		arrayListOf("the agreement", "die Vereinbarung"),
+		arrayListOf("ahead", "voraus"),
+		arrayListOf("the air", "die Luft"),
+		arrayListOf("all", "alle"),
+		arrayListOf("allow", "erlauben"),
+		arrayListOf("almost", "fast"),
+		arrayListOf("alone", "allein"),
+		arrayListOf("along", "entlang"),
+		arrayListOf("already", "bereits"),
+		arrayListOf("also", "Auch"),
+		arrayListOf("although", "Obwohl"),
+		arrayListOf("always", "stets"),
+		arrayListOf("the American", "die Amerikaner"),
+		arrayListOf("among", "unter"),
+		arrayListOf("the amount", "die Summe"),
+		arrayListOf("the analysis", "Die Analyse"),
+		arrayListOf("and", "Und"),
+		arrayListOf("the animal", "das Tier"),
+		arrayListOf("another", "ein anderer"),
+		arrayListOf("the answer", "die Antwort"),
+		arrayListOf("any", "beliebig"),
+		arrayListOf("anyone", "irgendjemand"),
+		arrayListOf("anything", "irgendetwas"),
+		arrayListOf("appear", "erscheinen"),
+		arrayListOf("apply", "anwenden"),
+		arrayListOf("the approach", "die Vorgehensweise"),
+		arrayListOf("the area", "das Gebiet"),
+		arrayListOf("argue", "argumentieren"),
+		arrayListOf("the arm", "der Arm"),
+		arrayListOf("around", "um"),
+		arrayListOf("arrive", "ankommen"),
+		arrayListOf("the art", "die Kunst"),
+		arrayListOf("the article", "der Artikel"),
+		arrayListOf("the artist", "der Künstler"),
+		arrayListOf("the as", "das as"),
+		arrayListOf("ask", "fragen"),
+		arrayListOf("assume", "annehmen"),
+		arrayListOf("the at", "das at"),
+		arrayListOf("the attack", "der Angriff"),
+		arrayListOf("the attention", "die Aufmerksamkeit"),
+		arrayListOf("the attorney", "der Anwalt"),
+		arrayListOf("the audience", "die Zuschauer"),
+		arrayListOf("the author", "der Autor"),
+		arrayListOf("the authority", "die Autorität"),
+		arrayListOf("available", "verfügbar"),
+		arrayListOf("avoid", "vermeiden"),
+		arrayListOf("away", "weg"),
+		arrayListOf("the baby", "das Baby"),
+		arrayListOf("the back", "der Rücken"),
+		arrayListOf("the bad", "das Schlechte"),
+		arrayListOf("the bag", "die Tasche"),
+		arrayListOf("the ball", "der Ball"),
+		arrayListOf("the bank", "die Bank"),
+		arrayListOf("the bar", "die Bar"),
+		arrayListOf("the base", "die Basis"),
+		arrayListOf("the be", "das Sein"),
+		arrayListOf("the beat", "der Beat"),
+		arrayListOf("beautiful", "Schön"),
+		arrayListOf("because", "Weil"),
+		arrayListOf("become", "werden"),
+		arrayListOf("the bed", "das Bett"),
+		arrayListOf("before", "Vor"),
+		arrayListOf("the begin", "der Anfang"),
+		arrayListOf("the behavior", "das Verhalten"),
+		arrayListOf("the behind", "der Hintern"),
+		arrayListOf("believe", "glauben"),
+		arrayListOf("the benefit", "der Vorteil"),
+		arrayListOf("the best", "der beste"),
+		arrayListOf("the better", "desto besser"),
+		arrayListOf("between", "zwischen"),
+		arrayListOf("beyond", "darüber hinaus"),
+		arrayListOf("big", "groß"),
+		arrayListOf("the bill", "die Rechnung"),
+		arrayListOf("the billion", "die Milliarden"),
     )
     private val unansweredQuestionAnswerPairsBuffer: ArrayList<ArrayList<String>> = ArrayList(unansweredQuestionAnswerPairs.shuffled().take(bufferSize))
     private val answeredQuestionAnswerPairs: ArrayList<ArrayList<String>> = arrayListOf()
@@ -67,7 +162,7 @@ class MainActivity : AppCompatActivity() {
         tts = TextToSpeech(applicationContext) {
             if (it == TextToSpeech.SUCCESS) {
                 tts.language = Locale.US
-                tts.setSpeechRate(1.0f)
+                tts.setSpeechRate(speechRate)
                 val params = HashMap<String, String>()
                 params[TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID] = "utteranceId"
                 tts.speak(question, TextToSpeech.QUEUE_ADD, params)
@@ -102,11 +197,32 @@ class MainActivity : AppCompatActivity() {
                     unansweredQuestionAnswerPairsBuffer.add(randomElement)
                     unansweredQuestionAnswerPairs.remove(randomElement)
                 }
-            }
-            unansweredQuestionAnswerPairsBuffer.add(unansweredQuestionAnswerPairsBuffer.removeFirst())
-            userAnswer = ""
-            askingSpeechInput = false
-            testQuestionAnswer()
+				unansweredQuestionAnswerPairsBuffer.add(removedElement)
+				userAnswer = ""
+				askingSpeechInput = false
+				testQuestionAnswer()
+            } else {
+				sayCorrectAnswer()
+				unansweredQuestionAnswerPairsBuffer.add(unansweredQuestionAnswerPairsBuffer.removeFirst())
+				userAnswer = ""
+				askingSpeechInput = false
+			}
         }
     }
+
+	private fun sayCorrectAnswer() {
+		val answer = unansweredQuestionAnswerPairsBuffer[0][1]
+		tts = TextToSpeech(applicationContext) {
+			if (it == TextToSpeech.SUCCESS) {
+				tts.language = Locale.GERMAN
+				tts.setSpeechRate(speechRate)
+				val params = HashMap<String, String>()
+				params[TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID] = "utteranceId"
+				tts.speak(answer, TextToSpeech.QUEUE_ADD, params)
+			}
+		}
+		tts.setOnUtteranceCompletedListener { utteranceId ->
+			testQuestionAnswer()
+		}
+	}
 }
