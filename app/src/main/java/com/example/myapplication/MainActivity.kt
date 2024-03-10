fc8f4ffbb80eec60dde0a55cf402793e863a580e
Tesla2000 <fratajczak124@gmail.com>
2024-03-10 21:44:19 +0100

    Tested on buffer size of 1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,47 +10,29 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.util.Locale
-import java.util.logging.Logger
+import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
     private val RQ_SPEECH_REC = 102
     private lateinit var tts: TextToSpeech
     private var userAnswer: String = ""
-    private val Log = Logger.getLogger(MainActivity::class.java.name)
     private var askingSpeechInput = false
-
+    private val repetitionRate = 0.5f
+    private val bufferSize = 1
+    private val unansweredQuestionAnswerPairs: ArrayList<ArrayList<String>> = arrayListOf(
+        arrayListOf("mother", "die mutter"),
+        arrayListOf("father", "der vater"),
+        arrayListOf("cat", "die katze"),
+        arrayListOf("dog", "der hund"),
+        arrayListOf("brother", "der Bruder"),
+        arrayListOf("sister", "die Schwester"),
+    )
+    private val unansweredQuestionAnswerPairsBuffer: ArrayList<ArrayList<String>> = ArrayList(unansweredQuestionAnswerPairs.shuffled().take(bufferSize))
+    private val answeredQuestionAnswerPairs: ArrayList<ArrayList<String>> = arrayListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.warning("Hello World")
-        setContentView(R.layout.activity_main)
-        var questionAnswerPairs: HashMap<String, String> = hashMapOf(
-            "mother" to "die mutter",
-//            "father" to "der vater",
-            )
-
-        for ((question, answer) in questionAnswerPairs) {
-            tts = TextToSpeech(applicationContext) {
-                if (it == TextToSpeech.SUCCESS) {
-                    tts.language = Locale.US
-                    tts.setSpeechRate(1.0f)
-                    val params = HashMap<String, String>()
-                    params[TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID] = "utteranceId"
-                    tts.speak(question, TextToSpeech.QUEUE_ADD, params)
-                }
-            }
-
-            tts.setOnUtteranceCompletedListener { utteranceId ->
-                if (utteranceId == "utteranceId") {
-                    do {
-                        if (!askingSpeechInput) {
-                            askingSpeechInput = true
-                            askSpeechInput()
-                        }
-                    } while (userAnswer.lowercase() != answer.lowercase())
-                }
-            }
-        }
+        testQuestionAnswer()
     }
 
     private fun askSpeechInput() {
@@ -76,6 +58,55 @@ class MainActivity : AppCompatActivity() {
 
             userAnswer = result?.get(0).toString()
             askingSpeechInput = false
+        }
+    }
+
+    private fun testQuestionAnswer() {
+        val question = unansweredQuestionAnswerPairsBuffer[0][0]
+        val answer = unansweredQuestionAnswerPairsBuffer[0][1]
+        tts = TextToSpeech(applicationContext) {
+            if (it == TextToSpeech.SUCCESS) {
+                tts.language = Locale.US
+                tts.setSpeechRate(1.0f)
+                val params = HashMap<String, String>()
+                params[TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID] = "utteranceId"
+                tts.speak(question, TextToSpeech.QUEUE_ADD, params)
+            }
+        }
+
+        tts.setOnUtteranceCompletedListener { utteranceId ->
+            do {
+                if (!askingSpeechInput) {
+                    askingSpeechInput = true
+                    askSpeechInput()
+                }
+            } while (userAnswer == "")
+            Log.i("USER", userAnswer)
+            Log.i("USER", answer)
+            if (userAnswer.lowercase() == answer.lowercase()) {
+                Log.i("USER", "Answer correct")
+                val removedElement = unansweredQuestionAnswerPairsBuffer.removeFirst()
+                if (!answeredQuestionAnswerPairs.contains(removedElement)) {
+                    Log.i("USER", "Adding $removedElement to answered")
+                    answeredQuestionAnswerPairs.add(removedElement)
+                } else {
+                    Log.i("USER", "$removedElement already in answered $answeredQuestionAnswerPairs")
+                }
+                if (unansweredQuestionAnswerPairs.isEmpty() || Random.nextFloat() < repetitionRate) {
+                    val randomElement = answeredQuestionAnswerPairs.random()
+                    Log.i("USER", "Adding $randomElement from answeredQuestionAnswerPairs to buffer")
+                    unansweredQuestionAnswerPairsBuffer.add(randomElement)
+                } else {
+                    val randomElement = unansweredQuestionAnswerPairs.random()
+                    Log.i("USER", "Adding $randomElement from unansweredQuestionAnswerPairs to buffer")
+                    unansweredQuestionAnswerPairsBuffer.add(randomElement)
+                    unansweredQuestionAnswerPairs.remove(randomElement)
+                }
+            }
+            unansweredQuestionAnswerPairsBuffer.add(unansweredQuestionAnswerPairsBuffer.removeFirst())
+            userAnswer = ""
+            askingSpeechInput = false
+            testQuestionAnswer()
         }
     }
 }
